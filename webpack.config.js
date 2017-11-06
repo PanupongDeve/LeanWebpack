47266a9d6fc19ff8bf9d9bc87edc6c08eb9c965e
PanupongDeve <5635512110@psu.ac.th>
2017-11-06 18:51:11 +0700

    add picture and font with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,30 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            //Load Css
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test:/\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test:/\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
     }
 };
 
